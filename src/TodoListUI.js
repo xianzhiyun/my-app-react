cf0904d1d324a01addb0aa012a36955789ec28b9
zhangle <zhangle@vtstar.net>
2020-05-08 22:06:50 +0800

    🚀feat(todolist): 将UI组件转化无状态组件提升性能

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,41 +1,39 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import 'antd/dist/antd.css';
 import {Input, Button, List} from 'antd';
 
-class TodoListUi extends Component {
-    render() {
-        return (
-            <Fragment>
-                <div style={{padding: '10px'}}>
-                    <div>
-                        <Input
-                            style={{width: '300px', marginRight: '20px'}}
-                            value={this.props.inputValue}
-                            onChange={this.props.changeInputValue}/>
-                        <Button
-                            onClick={this.props.clickBtn}
-                            type="primary"
-                        >添加的</Button>
-                    </div>
-                    <div style={{marginTop: '10px'}}>
-                        <List
-                            bordered
-                            dataSource={this.props.list}
-                            renderItem={(item, index) => (
-                                <List.Item
-                                    onClick={() => {
-                                        this.props.deleteItem(index)
-                                    }}
-                                >
-                                    {item}
-                                </List.Item>
-                            )}
-                        />
-                    </div>
+function TodoListUi(props) {
+    return (
+        <Fragment>
+            <div style={{padding: '10px'}}>
+                <div>
+                    <Input
+                        style={{width: '300px', marginRight: '20px'}}
+                        value={props.inputValue}
+                        onChange={props.changeInputValue}/>
+                    <Button
+                        onClick={props.clickBtn}
+                        type="primary"
+                    >添加的</Button>
                 </div>
-            </Fragment>
-        )
-    }
+                <div style={{marginTop: '10px'}}>
+                    <List
+                        bordered
+                        dataSource={props.list}
+                        renderItem={(item, index) => (
+                            <List.Item
+                                onClick={() => {
+                                    props.deleteItem(index)
+                                }}
+                            >
+                                {item}
+                            </List.Item>
+                        )}
+                    />
+                </div>
+            </div>
+        </Fragment>
+    )
 }
 
 export default TodoListUi;
